9adac9ead344562a9b1086013e2f3ecc41ba50f7
ksllive <zhenya-kiselev-97@mail.ru>
2020-09-25 11:23:38 +0300

    add session

--- DownloadImage/ViewController.swift
+++ DownloadImage/ViewController.swift
@@ -18,6 +18,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getImagePressed(_ sender: Any) {
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            
+            if let data = data, let image = UIImage(data: data) {
+                
+                DispatchQueue.main.async {
+                    self.label.isHidden = true
+                    self.getImageButton.isEnabled = false
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
     }
 }
 
