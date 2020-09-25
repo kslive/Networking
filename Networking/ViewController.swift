aae5ea0abba6df99750fc05c3dce29fae326fd46
ksllive <zhenya-kiselev-97@mail.ru>
2020-09-25 13:52:49 +0300

    update project.

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  DownloadImage
+//  Networking
 //
 //  Created by Eugene Kiselev on 25.09.2020.
 //
@@ -8,41 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var getImageButton: UIButton!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func getRequest(_ sender: Any) {
         
-        activityIndicator.isHidden = true
-        activityIndicator.hidesWhenStopped = true
     }
-
-    @IBAction func getImagePressed(_ sender: Any) {
+    
+    @IBAction func postRequest(_ sender: Any) {
         
-        label.isHidden = true
-        getImageButton.isEnabled = false
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
-        
-        let session = URLSession.shared
-        
-        session.dataTask(with: url) { (data, response, error) in
-            
-            if let data = data, let image = UIImage(data: data) {
-                
-                DispatchQueue.main.async {
-                
-                    self.activityIndicator.stopAnimating()
-                    self.imageView.image = image
-                }
-            }
-        }.resume()
     }
 }
-
