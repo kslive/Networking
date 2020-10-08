c7b759bf02ddf6965cae2fa4dc64c9845e817f22
ksllive <zhenya-kiselev-97@mail.ru>
2020-10-08 11:48:41 +0300

    background download, part 2, + alert/progress view.

--- Networking/Network/DataProvider.swift
+++ Networking/Network/DataProvider.swift
@@ -70,7 +70,7 @@ extension DataProvider: URLSessionDownloadDelegate {
         
         guard totalBytesExpectedToWrite != NSURLSessionTransferSizeUnknown else { return }
         
-        let progress = Double(totalBytesWritten/totalBytesExpectedToWrite)
+        let progress = Double(totalBytesWritten)/Double(totalBytesExpectedToWrite)
         
         print("download progress: \(progress)")
         
